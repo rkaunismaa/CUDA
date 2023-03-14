7c672c897bee5558f489d996d7272b194c4fb2e4
rkaunismaa <rkaunis@gmail.com>
2023-03-14 12:21:01 -0400

    formatting improvements

--- CUDA-Training-Series/hw1/vector_add_solution.cu
+++ CUDA-Training-Series/hw1/vector_add_solution.cu
@@ -18,7 +18,9 @@
 //const int DSIZE = 8192;
 //const int DSIZE = 16384;
 //const int DSIZE = 65536;
-const int DSIZE = 4194304;
+//const int DSIZE = 4194304;    // 2 ** 22
+//const int DSIZE = 8388608;    // 2 ** 23
+const int DSIZE = 4294967296;  // 2 ** 32
 const int block_size = 256;  // CUDA maximum is 1024
 // vector add kernel: C = A + B
 __global__ void vadd(const float *A, const float *B, float *C, int ds){
@@ -34,27 +36,35 @@ int main(){
   h_A = new float[DSIZE];
   h_B = new float[DSIZE];
   h_C = new float[DSIZE];
+
   for (int i = 0; i < DSIZE; i++){
     h_A[i] = rand()/(float)RAND_MAX;
     h_B[i] = rand()/(float)RAND_MAX;
     h_C[i] = 0;}
+
   cudaMalloc(&d_A, DSIZE*sizeof(float));
   cudaMalloc(&d_B, DSIZE*sizeof(float));
   cudaMalloc(&d_C, DSIZE*sizeof(float));
   cudaCheckErrors("cudaMalloc failure");
+
   cudaMemcpy(d_A, h_A, DSIZE*sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_B, h_B, DSIZE*sizeof(float), cudaMemcpyHostToDevice);
   cudaCheckErrors("cudaMemcpy H2D failure");
+
   //cuda processing sequence step 1 is complete
   vadd<<<(DSIZE+block_size-1)/block_size, block_size>>>(d_A, d_B, d_C, DSIZE);
   cudaCheckErrors("kernel launch failure");
+
   //cuda processing sequence step 2 is complete
   cudaMemcpy(h_C, d_C, DSIZE*sizeof(float), cudaMemcpyDeviceToHost);
   //cuda processing sequence step 3 is complete
   cudaCheckErrors("kernel execution failure or cudaMemcpy H2D failure");
+
   printf("A[0] = %f\n", h_A[0]);
   printf("B[0] = %f\n", h_B[0]);
   printf("C[0] = %f\n", h_C[0]);
+
   return 0;
+  
 }
   
