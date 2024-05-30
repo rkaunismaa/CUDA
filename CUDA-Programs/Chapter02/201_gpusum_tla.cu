f546761ca21a50b68440dddaf726bf9fd3674b34
rkaunismaa <rkaunis@gmail.com>
2024-05-30 11:34:20 -0400

    discovering more differences between the book and the code

--- CUDA-Programs/Chapter02/201_gpusum_tla.cu
+++ CUDA-Programs/Chapter02/201_gpusum_tla.cu
@@ -44,11 +44,11 @@ __global__ void gpu_sin_tla_forloop(float *sums,int steps,int terms,float step_s
 int main(int argc,char *argv[])
 {
 	if (argc < 2) {
-		printf("usage gpusum_tla steps|1000000 terms|1000 threads|256 blocks|256 loopkind|1\n");
+		printf("usage gpusum_tla steps|10000000 terms|1000 threads|256 blocks|256 loopkind|1\n");
 		return 0;
 	}
 	
-	int steps   = (argc > 1) ? atoi(argv[1])  : 1000000;
+	int steps   = (argc > 1) ? atoi(argv[1])  : 10000000;
 	int terms   = (argc > 2) ? atoi(argv[2])  : 1000;
 	int threads = (argc > 3) ? atoi(argv[3])  : 256;
 	int blocks  = (argc > 4) ? atoi(argv[4])  : (steps+threads-1)/threads;  // ensure threads*blocks >= steps
@@ -61,18 +61,22 @@ int main(int argc,char *argv[])
 	float *dptr = thrust::raw_pointer_cast(&dsums[0]); // get pointer    
 
 	cx::timer tim;                  // declare and start timer
+
 	if (loopkind== 1) {
 		gpu_sin_tla_whileloop<<<blocks,threads>>>(dptr,steps,terms,(float)step_size);  // tla using while loop
 	}
 	else {
 		gpu_sin_tla_forloop<<<blocks,threads>>>(dptr,steps,terms,(float)step_size);  // tla using for loop
 	}
+
 	double gpu_sum = thrust::reduce(dsums.begin(),dsums.end());
+
 	double gpu_time = tim.lap_ms(); // get elapsed time
 
 	double rate = (double)steps*(double)terms/(gpu_time*1000000.0);
 	gpu_sum -= 0.5*(sinsum(0.0f,terms)+sinsum(pi,terms));
 	gpu_sum *= step_size;
+
 	if (loopkind==1)printf("gpu_sum while loop sum = %.10f, steps %d terms %d time %.3f ms config %7d %4d rate %f \n",gpu_sum,steps,terms,gpu_time,blocks,threads,rate);
 	else            printf("gpu_sum   for loop sum = %.10f, steps %d terms %d time %.3f ms config %7d %4d rate %f \n",gpu_sum,steps,terms,gpu_time,blocks,threads,rate);
 
