a800b1f596345afcbfff08902dce936fb3184f3c
rkaunismaa <rkaunis@gmail.com>
2024-05-31 12:49:28 -0400

    CUDA-Programs/Chapter02/203_grid3D.cu

--- CUDA-Programs/Chapter02/203_grid3D.cu
+++ CUDA-Programs/Chapter02/203_grid3D.cu
@@ -44,40 +44,70 @@
 __device__  int   a[256][512][512];  // file scope
 __device__  float b[256][512][512];  // file scope
 
-__global__ void grid3D(int nx,int ny,int nz,int id)
+//                         512     512     256     1234567
+__global__ void grid3D(int nx, int ny, int nz, int id)
 {
-	int x = blockIdx.x*blockDim.x+threadIdx.x; // find (x,y,z) in
-	int y = blockIdx.y*blockDim.y+threadIdx.y; // in arrays
-	int z = blockIdx.z*blockDim.z+threadIdx.z; // 
+
+	// dim3 block3d(16, 64, 128); // 16*64*128 = 131072
+	// dim3 thread3d(32, 8, 2); // 32*8*2    = 512
+	// grid3D<<<block3d, thread3d>>>(512, 512, 256, id);
+
+	// int x = blockIdx.x*blockDim.x + threadIdx.x; // find (x,y,z) in
+	// int y = blockIdx.y*blockDim.y + threadIdx.y; // in arrays
+	// int z = blockIdx.z*blockDim.z + threadIdx.z; // 
+
+	int blockDimx = blockDim.x; // 32
+	int blockDimy = blockDim.y; // 8
+	int blockDimz = blockDim.z; // 2
+
+	if(blockDimx != 32 || blockDimy != 8 || blockDimz != 2)
+	{
+		printf("KABOOM!");
+		return;     // out of range?
+	};
+
+	int x = blockIdx.x*blockDimx + threadIdx.x; // find (x,y,z) in
+	int y = blockIdx.y*blockDimy + threadIdx.y; // in arrays
+	int z = blockIdx.z*blockDimz + threadIdx.z; // 
+
 	if(x >=nx || y >=ny || z >=nz) return;     // out of range?
 
 	int array_size = nx*ny*nz;
 	int block_size = blockDim.x*blockDim.y*blockDim.z;
-	int grid_size  =  gridDim.x* gridDim.y* gridDim.z;
+	int grid_size  = gridDim.x* gridDim.y* gridDim.z;
+
 	int total_threads = block_size*grid_size;
-	int thread_rank_in_block = (threadIdx.z*blockDim.y+threadIdx.y)*blockDim.x+threadIdx.x;
-	int block_rank_in_grid  =  (blockIdx.z*gridDim.y+ blockIdx.y)*gridDim.x+ blockIdx.x;
+
+	int thread_rank_in_block = (threadIdx.z*blockDim.y + threadIdx.y)*blockDim.x + threadIdx.x;
+	int block_rank_in_grid  =  (blockIdx.z*gridDim.y + blockIdx.y)*gridDim.x + blockIdx.x;
 	int thread_rank_in_grid = thread_rank_in_block + block_size*block_rank_in_grid;
 
 	// do some work here
 	a[z][y][x] = thread_rank_in_grid;
 	b[z][y][x] = sqrtf((float)a[z][y][x]);
+
 	if(thread_rank_in_grid == id) {
-		printf("array size   %3d x %3d x %3d = %d\n",nx,ny,nz,array_size);
-		printf("thread block %3d x %3d x %3d = %d\n",blockDim.x,blockDim.y,blockDim.z,block_size);
-		printf("thread  grid %3d x %3d x %3d = %d\n",gridDim.x,gridDim.y,gridDim.z,grid_size);
-		printf("total number of threads in grid %d\n",total_threads);
-		printf("a[%d][%d][%d] = %i and b[%d][%d][%d] = %f\n",z,y,x,a[z][y][x],z,y,x,b[z][y][x]);
-		printf("rank_in_block = %d rank_in_grid = %d rank of block_rank_in_grid = %d\n",thread_rank_in_block,thread_rank_in_grid,block_rank_in_grid);
+		printf("*** START ***\n");
+		printf("array size   %3d x %3d x %3d = %d\n", nx, ny, nz, array_size);
+		printf("thread block %3d x %3d x %3d = %d\n", blockDim.x, blockDim.y, blockDim.z, block_size);
+		printf("thread  grid %3d x %3d x %3d = %d\n", gridDim.x, gridDim.y, gridDim.z, grid_size);
+		printf("total number of threads in grid %d\n", total_threads);
+		printf("a[%d][%d][%d] = %i and b[%d][%d][%d] = %f\n", z, y, x, a[z][y][x], z, y, x, b[z][y][x]);
+		printf("rank_in_block = %d rank_in_grid = %d rank of block_rank_in_grid = %d\n", thread_rank_in_block, thread_rank_in_grid, block_rank_in_grid);
+		printf("*** END ***\n");
 	}
 }
 
 int main(int argc,char *argv[])
 {
 	int id = (argc > 1) ? atoi(argv[1]) : 12345;
-	dim3 thread3d(32,8,2); // 32*8*2    = 512
-	dim3  block3d(16,64,128); // 16*64*128 = 131072
-	grid3D<<<block3d,thread3d>>>(512,512,256,id);
+
+	dim3 block3d(16, 64, 128); // 16*64*128 = 131072
+	dim3 thread3d(32, 8, 2); // 32*8*2    = 512
+
+	grid3D<<<block3d, thread3d>>>(512, 512, 256, id);
+
     cudaDeviceSynchronize(); // necessary in Linux to see kernel printf
+
 	return 0;
 }
