227e7c5517fb1583aa206b103d87771ca006b23d
rkaunismaa <rkaunis@gmail.com>
2024-06-06 16:41:21 -0400

    CUDA-Programs/Chapter02/205_reduce0.cu

--- CUDA-Programs/Chapter02/205_reduce0.cu
+++ CUDA-Programs/Chapter02/205_reduce0.cu
@@ -18,25 +18,31 @@
 
 __global__ void reduce0(float *x,int m)
 {
-	int tid = blockDim.x*blockIdx.x+threadIdx.x;
+	int tid = (blockDim.x * blockIdx.x) + threadIdx.x;
 	x[tid] += x[tid+m];
 }
 
 int main(int argc,char *argv[])
 {
 	int N = (argc> 1) ? atoi(argv[1]) : 1 << 24; // default 224
+
+	// N => 16,777,216
 	thrust::host_vector<float>       x(N);
 	thrust::device_vector<float> dev_x(N);
 
 	// initialise x with random numbers and copy to dx
 	std::default_random_engine gen(12345678);
-	std::uniform_real_distribution<float> fran(0.0,1.0);
+	std::uniform_real_distribution<float> fran(0.0, 1.0);
+
 	for(int k = 0; k<N; k++) x[k] = fran(gen);
 
 	dev_x = x;  // H2D copy (N words)
+
 	cx::timer tim;
+
 	double host_sum = 0.0;
 	for(int k = 0; k<N; k++) host_sum += x[k]; // host reduce!
+
 	double t1 = tim.lap_ms();
 
 	// simple GPU reduce for N = power of 2  
@@ -50,6 +56,6 @@ int main(int argc,char *argv[])
 	double t2 = tim.lap_ms();
 
 	double gpu_sum = dev_x[0];  // D2H copy (1 word)
-	printf("sum of %d random numbers: host %.1f %.3f ms, GPU %.1f %.3f ms\n",N,host_sum,t1,gpu_sum,t2);
+	printf("sum of %d random numbers: host %.1f %.3f ms, GPU %.1f %.3f ms\n", N, host_sum, t1, gpu_sum, t2);
 	return 0;
 }
