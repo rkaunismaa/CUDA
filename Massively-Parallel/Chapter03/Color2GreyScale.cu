55064a2d1f08990b4a79a6634965f47e5856cce0
rkaunismaa <rkaunis@gmail.com>
2024-06-10 17:47:32 -0400

    Massively-Parallel/Chapter03/Color2GreyScale.cu

--- Massively-Parallel/Chapter03/Color2GreyScale.cu
+++ Massively-Parallel/Chapter03/Color2GreyScale.cu
@@ -31,22 +31,32 @@ void getImageDimensions(const std::string& filename, int& width, int& height) {
     height = image.rows;
 }
 
-
+// CUDA kernel (simple example)
+__global__ void processImageKernel(unsigned char* d_image, int width, int height) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x < width && y < height) {
+        // Example: Invert the color
+        int index = y * width + x;
+        d_image[index] = 255 - d_image[index];
+    }
+}
 
 int main(int argc, char * argv[])
 {
 
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <image_file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <image_file>, <processed_file>" << std::endl;
         return -1;
     }
 
     std::string filename = argv[1];
+    //std::string processedFilename = argv[2];
 
-    if (!fileExists(filename)) {
-        std::cerr << "File does not exist: " << filename << std::endl;
-        return -1;
-    }
+    // if (!fileExists(filename)) {
+    //     std::cerr << "File does not exist: " << filename << std::endl;
+    //     return -1;
+    // }
 
     int width, height;
     try {
@@ -56,6 +66,37 @@ int main(int argc, char * argv[])
         std::cerr << e.what() << std::endl;
         return -1;
     }
+
+     cv::Mat image = cv::imread(filename, cv::IMREAD_GRAYSCALE);
+    if (image.empty()) {
+        std::cerr << "Failed to load image." << std::endl;
+        return -1;
+    }
+
+    // Allocate CUDA memory
+    unsigned char* d_image;
+    size_t imageSize = width * height * sizeof(unsigned char);
+    cudaMalloc(&d_image, imageSize);
+
+    // Copy image data to CUDA memory
+    cudaMemcpy(d_image, image.data, imageSize, cudaMemcpyHostToDevice);
+
+    // Launch the CUDA kernel
+    dim3 blockSize(16, 16);
+    dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+    processImageKernel<<<gridSize, blockSize>>>(d_image, width, height);
+
+    // Copy result back to host
+    cudaMemcpy(image.data, d_image, imageSize, cudaMemcpyDeviceToHost);
+
+    // Save the processed image
+    std::string processedFilename = "/home/rob/Data/Documents/Github/rkaunismaa/CUDA/images/GregLemond_BernardHinault_2.jpg" ;
+    cv::imwrite(processedFilename, image);
+
+    // Clean up
+    cudaFree(d_image);
+
+    return 0;
 
 
     
